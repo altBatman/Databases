eb150c7f875ae9eb05cbcb7df31b49df9a54f434
Rajesh Mangena <rajeshinap@gmail.com>
2019-01-21 06:02:26 +0100

    GUI Bookstore database V1

---
+++ bookstore_database/README.md
@@ -0,0 +1,7 @@
+This a gui Database build for a bookstore.
+
+The Database is stored in local .db file!
+
+Code can be tweaked to apply for any dbms
+
+The goal is to build a GUI for a database.
--- bookstore_database/backend.py
+++ bookstore_database/backend.py
@@ -1,41 +1,46 @@
-import psycopg2
+import sqlite3
 
-def create_table():
-    conn= psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
+def connect():
+    conn= sqlite3.connect("books.db")
     cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT, quantity INTEGER, price REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
     conn.commit()
     conn.close()
 
-def insert(item,quantity,price):
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
+def insert(title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
     cur=conn.cursor()
-    cur.execute("INSERT INTO store VALUES(%s,%s,%s)",(item,quantity,price))
+    cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,author,year,isbn))
     conn.commit()
     conn.close()
 
 def view():
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
+    conn=sqlite3.connect("books.db")
     cur=conn.cursor()
-    cur.execute("SELECT * FROM store")
+    cur.execute("SELECT * FROM book")
     rows=cur.fetchall()
     conn.close()
     return rows
-def delete(item):
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
+
+def search(title="",author="",year="",isbn=""):
+    conn=sqlite3.connect("books.db")
     cur=conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows=cur.fetchall()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
     conn.commit()
     conn.close()
 
-def update(quantity,price,item):
-    conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
+def update(id,title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
     cur=conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
     conn.commit()
     conn.close()
 
-
-#insert("Milk", 4, 8)
-#update(2,6.0,"Milk")
-#print(view())
+connect()
